8cb934a6129d33ab095365f6b809abbec8160bbe
FulmineGiallo <39560628+FulmineGiallo@users.noreply.github.com>
2018-11-27 22:56:52 +0100

    Initial commit

---
+++ es3/script.js
@@ -0,0 +1,244 @@
+var book= 
+[
+  {
+    "id"  : "32958",
+    "genere" : " Informatica",
+    "autore" : " Piero Gallo, Fabio Salerno",
+    "editore" : "Mondadori",
+    "nome"   : " Cloud",
+    "data"   : " 2015",
+    "prezzo" : " 25euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+  },
+  {
+    "id"  : "93346",
+    "genere" : " Informatica",
+    "autore" : " Bjarne Stroustrup",
+    "editore" : "Mondadori",
+    "nome"   : " C++",
+    "data"   : " 2015",
+    "prezzo" : " 50euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+  },
+  {
+    "id"  : "83942",
+    "genere" : " Informatica",
+    "autore" : " Andrew Hodges ",
+    "editore" : "Bollari Boringhieri",
+    "nome"   : " Alan Turing: The Imitation Game",
+    "data"   : " 2014",
+    "prezzo" : " 35euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+  },
+  {
+    "id"  : "43952",
+    "genere" : " Informatica",
+    "autore" : " Alessandra Salvaggio",
+    "editore" : "Mondadori",
+    "nome"   : " HTML5 e CSS3",
+    "data"   : " 2015",
+    "prezzo" : " 26euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "38731",
+    "genere" : " Fantasy",
+    "autore" : " George R. R. Martin",
+    "editore" : "Mondadori",
+    "nome"   : " Game of Throne",
+    "data"   : " 2017",
+    "prezzo" : " 50euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+  },
+  {
+    "id"  : "64853",
+    "genere" : " Fantasy",
+    "autore" : " Suzanne Collins",
+    "editore" : "Mondadori",
+    "nome"   : " Hunger Games",
+    "data"   : " 2008",
+    "prezzo" : " 20euro",
+    "condizione" : "usato",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "12795",
+    "genere" : " Fantasy",
+    "autore" : " Rick Riordan",
+    "editore" : "Mondadori",
+    "nome"   : " Percy Jackson",
+    "data"   : " 2013",
+    "prezzo" : " 30euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+  },
+  {
+    "id"  : "36871",
+    "genere" : " Fantasy",
+    "autore" : " J. K. Rowling",
+    "editore" : "Salani Editore",
+    "nome"   : " Animali fantastici e dove trovarli",
+    "data"   : " 2001",
+    "prezzo" : " 45euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "17892",
+    "genere" : " Horror",
+    "autore" : " Stephen King",
+    "editore" : "Sperling Paperback",
+    "nome"   : " IT",
+    "data"   : " 2017",
+    "prezzo" : " 30euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "82346",
+    "genere" : " Horror",
+    "autore" : " Stephen King",
+    "editore" : "Bompiani",
+    "nome"   : " Shining",
+    "data"   : " 1977",
+    "prezzo" : " 14euro",
+    "condizione" : "usato"
+ },
+ {
+    "id"  : "97621",
+    "genere" : " Horror",
+    "autore" : " Stephen King",
+    "editore" : "Sperling & Kupfer",
+    "nome"   : " The mist",
+    "data"   : " 1980",
+    "prezzo" : " 20euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "46982",
+    "genere" : " Horror",
+    "autore" : " Stephen King",
+    "editore" : "Sperling & Paperback",
+    "nome"   : " Cimitero vivente",
+    "data"   : " 1983",
+    "prezzo" : " 27euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+ },
+ {
+    "id"  : "69743",
+    "genere" : " Dramma",
+    "autore" : " Khaled Hosseini",
+    "editore" : "Piemme",
+    "nome"   : " Il cacciatore di aquiloni",
+    "data"   : " 2003",
+    "prezzo" : " 24euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+},
+{
+    "id"  : "75315",
+    "genere" : " Fantasy",
+    "autore" : " J. K. Rowling",
+    "editore" : "Salani Editore",
+    "nome"   : " Harry Potter e il Prigioniero di Azkaban",
+    "data"   : " 2004",
+    "prezzo" : " 21 euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+},
+{
+    "id"  : "98522",
+    "genere" : " Dramma",
+    "autore" : " Jojo Moyes",
+    "editore" : "Mondadori",
+    "nome"   : " Io prima di te",
+    "data"   : " 2017",
+    "prezzo" : " 19euro",
+    "condizione" : "usato"
+},
+{
+  "id"  : "93497",
+  "genere" : " Dramma",
+  "autore" : " Stephen King",
+  "editore" : "Sperling & Paperback",
+  "nome"   : " Le ali della libertà",
+  "data"   : " 1995",
+  "prezzo" : " 30euro",
+  "condizione" : "nuovo",
+  "lingua" : "Inglese"
+},
+{
+    "id"  : "75246",
+    "genere" : " Giallo",
+    "autore" : " Agatha Christie",
+    "editore" : "Mondadori",
+    "nome"   : " Assasinio sull'Orient Express",
+    "data"   : " 1934",
+    "prezzo" : " 29euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+},
+{
+    "id"  : "54378",
+    "genere" : " Giallo",
+    "autore" : " Agatha Christie",
+    "editore" : "Mondadori",
+    "nome"   : " Dieci piccoli indiani",
+    "data"   : " 1939",
+    "prezzo" : " 40euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+},
+{
+    "id"  : "34791",
+    "genere" : " Giallo",
+    "autore" : " Arthur Conan Doyle",
+    "editore" : "Mondadori",
+    "nome"   : " Il mastino di baskerville",
+    "data"   : " 1902",
+    "prezzo" : " 18euro",
+    "condizione" : "usato",
+    "lingua" : "Inglese"
+},
+{
+    "id"  : "16792",
+    "genere" : " Giallo",
+    "autore" : " Stephen King",
+    "editore" : "Rizzoli",
+    "nome"   : " The outsider",
+    "data"   : " 2017",
+    "prezzo" : " 35euro",
+    "condizione" : "nuovo",
+    "lingua" : "Inglese"
+}];
+for(var i=0; i<book.length; i++)
+{
+  if(book[i].genere===" Informatica")
+  {
+    var div = document.createElement('div');
+    div.setAttribute('class', 'container');
+
+    var img = document.createElement("img");
+    img.setAttribute('src','libri/'+ i +'.jpg')
+
+    var overlay= document.createElement('div');
+    overlay.setAttribute('class','overlay');
+
+    var text= document.createElement('div');
+    text.setAttribute('class','text');
+
+    text.innerHTML='ID: ' + book[i].id +   "<br>" + "Genere: " + book[i].genere + "<br>" + "Editore: " + book[i].editore + "<br>" + "Autore: " + book[i].autore + "<br>" + "Titolo: " + book[i].nome + "<br>" + "Data: " + book[i].data + "<br>" + "Prezzo: " + book[i].prezzo + "<br>" + "Condizione: " + book[i].condizione +"<br>" + "Lingua originale: " + book[i].lingua;
+
+    document.body.appendChild(div);
+    div.appendChild(img);
+    overlay.appendChild(text);
+    div.appendChild(overlay);
+  }
+}
